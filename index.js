704744dec074bbb46862f1b8174f4aea95b8356c
Nick Hart <nhartmusic@gmail.com>
2020-06-19 17:43:41 +0200

    basic layout, scales and axes set up

---
+++ index.js
@@ -0,0 +1,44 @@
+//DOM elements 
+
+const btns = document.querySelectorAll('button')
+const form = document.querySelector('form')
+const formActivity = document.querySelector('form span')
+const input = document.querySelector('input')
+const error = document.querySelector('error')
+
+let activity = 'hiking'
+
+btns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        activity = e.target.dataset.activity
+
+        //switch active class to clicked button
+        btns.forEach(btn => btn.classList.remove('active'))
+        e.target.classList.add('active')
+
+        input.setAttribute('id', activity)
+
+        formActivity.textContent = activity
+
+    })
+})
+
+//form submit 
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const distance = parseInt(input.value)
+    if (distance) {
+        db.collection('activities').add({
+            distance,
+            activity,
+            date: new Date().toString()
+        }).then((error) => {
+            error.textContent = ''
+            input.value = ''
+        })
+    } else {
+        // error.textContent = 'Please enter a valid distance amount'
+    }
+})
+
